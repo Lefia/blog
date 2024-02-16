5a686a0cf1ef5049cd7667a386e249513796a8b4
Lefia <lefia1222@gmail.com>
2024-02-16 20:20:37 +0800

    ✨ Add NotFound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { BaseLayout } from '@/layout/BaseLayout'
+
+const page = () => {
+  return (
+    <BaseLayout title='404 - Page Not Found'>
+    </BaseLayout>
+  )
+}
+
+export default page
