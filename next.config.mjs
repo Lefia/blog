1a94a40a013a282326efcc3ec398249786de3aac
Lefia <lefia1222@gmail.com>
2024-02-21 23:42:15 +0800

    🐛 Use remotePatterns instead of domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import withPlaiceholder from "@plaiceholder/next";
+import withPlaiceholder from '@plaiceholder/next'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    },
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**'
+      }
+    ]
+  }
+}
 
-export default withPlaiceholder(nextConfig);
+export default withPlaiceholder(nextConfig)
