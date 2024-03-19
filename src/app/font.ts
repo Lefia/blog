a507bbdbfab399e7de3c9aeb3dc11cb22ce23473
Lefia <lefia1222@gmail.com>
2024-03-20 00:23:49 +0800

    ✨ Add Hero Section

--- src/app/font.ts
+++ src/app/font.ts
@@ -1,10 +1,15 @@
-import { Ubuntu_Mono, Inter } from 'next/font/google'
+import { Ubuntu_Mono, Inter, Exo_2 } from 'next/font/google'
 
 export const ubuntu_mono = Ubuntu_Mono({
   subsets: ['latin'],
   weight: '400'
 })
 
-export const inter = Inter({ 
-  subsets: ['latin'] 
+export const inter = Inter({
+  subsets: ['latin']
+})
+
+export const exo_2 = Exo_2({
+  subsets: ['latin'],
+  weight: '400'
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,31 @@ import {
 import PostCard from '@/components/PostCard'
 import { getPosts } from '@/lib/mdx'
 import Link from 'next/link'
+import Img from '@/components/Img'
+import BaseLayout from '@/components/BaseLayout'
+import { exo_2 } from '@/app/font'
+import { cn } from '@/lib/utils'
 
 const Home = async () => {
   return (
     <>
-      <Link href='/blog'>
-        <h2 className='text-2xl mb-4 inline-block'>Latest Post</h2>
-      </Link>
-      <PostCarousel className='mb-4' />
+      <div className='relative'>
+        <Img
+          src='/images/hero.jpg'
+          alt='hero-image'
+          className='w-full min-w-[512px] h-[calc(50vh-64px)] object-cover object-center md:brightness-75 brightness-50'
+        />
+        <div className={cn(exo_2.className, 'absolute left-[20%] top-[40%]')}>
+          <h1 className='text-4xl mb-2'>Hi!&nbsp;I&apos;m Lefia</h1>
+          <p className='text-xl'>A Sophomore student at NCU</p>
+        </div>
+      </div>
+      <BaseLayout>
+        <Link href='/blog'>
+          <h2 className='text-2xl mb-4 inline-block'>Latest Post</h2>
+        </Link>
+        <PostCarousel className='mb-4' />
+      </BaseLayout>
     </>
   )
 }
