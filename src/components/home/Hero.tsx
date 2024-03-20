4a07c4be86bca9021533197ee0fff70fe63577ef
Lefia <lefia1222@gmail.com>
2024-03-20 18:09:37 +0800

    ✨ Update Hero styles for mobile

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -16,18 +16,19 @@ const Hero = (props: HeroProps) => {
       )}
     >
       <Img
-        src='/images/hero.jpg'
+        src='https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=80&w=2070'
         alt='hero-image'
         className='min-w-[512px] h-[calc(50vh)] object-cover object-center md:brightness-75 brightness-50'
       />
       <div
         className={cn(
           exo_2.className,
-          'absolute left-[20%] top-[40%] text-white'
+          'absolute text-white left-1/2 top-1/2 -translate-x-[50%] md:left-[20%] md:top-[40%] md:translate-x-0 md:translate-y-[50%]',
+          'flex flex-col items-center md:items-start'
         )}
       >
         <h1 className='text-4xl mb-2'>Hi!&nbsp;I&apos;m Lefia</h1>
-        <p className='text-xl'>A Sophomore student at NCU</p>
+        <p className='text-lg md:text-xl'>A Sophomore student at NCU</p>
       </div>
     </div>
   )
